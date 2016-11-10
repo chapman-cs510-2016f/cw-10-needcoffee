206c88641d8dc8942072a344c4419408e1e87922
Alexis Ford <ford128@mail.chapman.edu>
2016-11-09 19:35:09 -0800

    fixed set and get functions

--- src/arrays/arrays.c
+++ src/arrays/arrays.c
@@ -22,10 +22,13 @@ int main(void)
       // Return a non-successful integer
       return -1;
     }
-    
-    set_arrays(array_size, array, memblock);
-    print_arrays(array_size, array, memblock);
    
+    char foo[array_size];
+ 
+    set_arrays(array_size, array, memblock, &foo);
+    print_arrays(array_size, array, memblock, &foo);
+   
+  
     // explicitly free the block of memory malloc-ed at memblock 
     free(memblock);
 
@@ -33,11 +36,12 @@ int main(void)
     return 0;
 }
 
-void set_arrays(int arraySize, float *anArray, long double *memBlock) {
+void set_arrays(int arraySize, float *anArray, long double *memBlock, char *fooBar) {
     
     // iteration index
     int i;
-    
+    char *foo = "Foo bar .";
+
     for (i=0; i < arraySize; i++)
     {
         // assign the value i to the ith element 
@@ -45,15 +49,15 @@ void set_arrays(int arraySize, float *anArray, long double *memBlock) {
         
         // set the value inside the memory address at memblock + (i bytes) to i
         *(memBlock+i) = i;
+
+        fooBar[i] = foo[i];
     }
 
 }
 
-void print_arrays(int arraySize, float * anArray, long double *memBlock) {
+void print_arrays(int arraySize, float * anArray, long double *memBlock, char *fooBar) {
     
  
-    const char *foo = "Foo bar .";
-
     int i;
 
     // increment three bytes BEYOND the allocated memory (buffer overrun)
@@ -65,10 +69,10 @@ void print_arrays(int arraySize, float * anArray, long double *memBlock) {
         // print the values contained in each memory address from memblock on
         printf("*(memblock + %d) : %Lf\t", i, *(memBlock+i));//changed second formatting from int to long float
         // print each character in the string
-        printf("foo[%d] : %c\t", i, foo[i]);
+        printf("foo[%d] : %c\t", i, fooBar[i]);
         
         // do the same thing, but in pointer notation
-        printf("*(foo + %d) : %c\n", i, *(foo+i));
+        printf("*(foo + %d) : %c\n", i, *(fooBar+i));
 
     }
 }
