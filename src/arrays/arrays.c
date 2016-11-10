6c18a28b1c16ff5814578d6b0972ca25c7a56523
Alexis Ford <ford128@mail.chapman.edu>
2016-11-09 19:24:46 -0800

    created set and print functions

--- src/arrays/arrays.c
+++ src/arrays/arrays.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void)
 {
+    
     int array_size = 10;
     
     // Declare an array of array_size integers in the usual way
-    int array[array_size];
+    float array[array_size]; //changed type from int to float
 
     // Allocate a block of array_size integers and assign the address
     // of the beginning of the memory block to the pointer array2
-    int *memblock = malloc(array_size * sizeof(int));
+    long double *memblock = malloc(array_size * sizeof(long double)); //changed *memblock type from int to long double; changed sizeof(int) to long double
     /* WARNING: malloc may fail and return a NULL value for the pointer
                 Good programming practice mandates checking for such failures.
     */
@@ -20,40 +22,54 @@ int main(void)
       // Return a non-successful integer
       return -1;
     }
-
-    // iteration index
-    int i;
-    // constant character array (i.e., a string)
-    const char *foo = "Foo bar .";
     
-    for (i=0; i < array_size; i++)
-    {
-        // assign the value i to the ith element 
-        array[i] = i;
-        
-        // set the value inside the memory address at memblock + (i bytes) to i
-        *(memblock+i) = i;
-    }
-
-    // increment three bytes BEYOND the allocated memory (buffer overrun)
-    for (i=0; i < (array_size + 3); i++)
-    {
-        // print the array values in the usual way
-        printf("array[%d] : %d\t", i, array[i]);
-        
-        // print the values contained in each memory address from memblock on
-        printf("*(memblock + %d) : %d\t", i, *(memblock+i));
-        
-        // print each character in the string
-        printf("foo[%d] : %c\t", i, foo[i]);
-        
-        // do the same thing, but in pointer notation
-        printf("*(foo + %d) : %c\n", i, *(foo+i));
-    }
-    
+    set_arrays(array_size, array, memblock);
+    print_arrays(array_size, array, memblock);
+   
     // explicitly free the block of memory malloc-ed at memblock 
     free(memblock);
 
     // memory not explicitly freed is automatically freed on function exit
     return 0;
 }
+
+void set_arrays(int arraySize, float *anArray, long double *memBlock) {
+    
+    // iteration index
+    int i;
+    
+    for (i=0; i < arraySize; i++)
+    {
+        // assign the value i to the ith element 
+        anArray[i] = i;
+        
+        // set the value inside the memory address at memblock + (i bytes) to i
+        *(memBlock+i) = i;
+    }
+
+}
+
+void print_arrays(int arraySize, float * anArray, long double *memBlock) {
+    
+ 
+    const char *foo = "Foo bar .";
+
+    int i;
+
+    // increment three bytes BEYOND the allocated memory (buffer overrun)
+    for (i=0; i < (arraySize + 3); i++)
+    {
+        // print the array values in the usual way
+        printf("array[%d] : %f\t", i, anArray[i]); //changed second formatting from int to float
+        
+        // print the values contained in each memory address from memblock on
+        printf("*(memblock + %d) : %Lf\t", i, *(memBlock+i));//changed second formatting from int to long float
+        // print each character in the string
+        printf("foo[%d] : %c\t", i, foo[i]);
+        
+        // do the same thing, but in pointer notation
+        printf("*(foo + %d) : %c\n", i, *(foo+i));
+
+    }
+}
+
